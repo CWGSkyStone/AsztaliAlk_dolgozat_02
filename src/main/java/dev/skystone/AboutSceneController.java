3879e52b537211889f42f757f97937493f80bc11
Szabo Jozsef <skystone213@gmail.com>
2026-04-21 13:51:59 +0200

    Feladat linkje és a megoldás linkje a névjegy színpadon már látható/használható)

--- src/main/java/dev/skystone/AboutSceneController.java
+++ src/main/java/dev/skystone/AboutSceneController.java
@@ -2,12 +2,24 @@ package dev.skystone;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import java.awt.Desktop;
+import java.io.IOException;
 
 public class AboutSceneController {
 
     @FXML
     void onClickBackButton(ActionEvent event) {
         App.setRoot("mainScene");
+    }
+    @FXML
+    void onClickVisitTask(ActionEvent event) throws IOException {
+        Desktop desktop = Desktop.getDesktop();
+        desktop.browse(java.net.URI.create("https://szit.hu/doku.php?id=oktatas:programozas:feladatok:altalanos#feladat_0327"));
+       }
+    @FXML
+    void onClickVisitSolution(ActionEvent event) throws IOException {
+        Desktop desktop = Desktop.getDesktop();
+        desktop.browse(java.net.URI.create("https://github.com/CWGSkyStone/AsztaliAlk_dolgozat_02"));
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module dev.skystone {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires java.desktop;
 
     opens dev.skystone to javafx.fxml;
     exports dev.skystone;
